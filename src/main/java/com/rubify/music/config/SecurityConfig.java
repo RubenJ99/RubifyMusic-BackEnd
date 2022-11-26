fd8f91afd71862ecfb3498ce5451c14bcabdbd9e
RubenJ99 <juarezperezruben13@gmail.com>
2022-11-26 16:34:05 +0100

    jwtFilter impl

--- src/main/java/com/rubify/music/config/SecurityConfig.java
+++ src/main/java/com/rubify/music/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.rubify.music.config;
 
 import com.rubify.music.service.UserDetailsServiceImpl;
 import com.rubify.music.utils.CustomPasswordEncoder;
+import com.rubify.music.utils.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,6 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsServiceImpl userDetailsService;
     @Autowired
     private CustomPasswordEncoder customPasswordEncoder;
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,6 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+      http = http.csrf().disable().cors().disable();
+      http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+      http = http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage());
+      }).and();
+      http.authorizeRequests().anyRequest().authenticated();
+      http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
--- src/main/java/com/rubify/music/utils/JwtFilter.java
+++ src/main/java/com/rubify/music/utils/JwtFilter.java
@@ -1,8 +1,16 @@
 package com.rubify.music.utils;
 
 import com.rubify.music.repository.IUserRepository;
+import com.rubify.music.service.UserDetailsServiceImpl;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -10,6 +18,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -20,6 +29,28 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //Header auth
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(StringUtils.hasText(header) || !header.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //get usr from token
+        final String token = header.split(" ")[1].trim();
+        UserDetails userDetails = userRepository.findByEmail(jwtUtil.getUsernameFromToken(token)).orElse(null);
+        //jwt token validation impl
+        if(!jwtUtil.validateToken(token,userDetails)) {
+            filterChain.doFilter(request,response);
+            return;
+        }
 
+        UsernamePasswordAuthenticationToken authentication
+                = new UsernamePasswordAuthenticationToken(
+                        userDetails,null,userDetails == null ? List.of() : userDetails.getAuthorities());
+
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request,response);
     }
 }
